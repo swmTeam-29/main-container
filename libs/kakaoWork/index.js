bcf9422f1b75ca56bc59c13b0aef2340586c55a1
Dongjin Lim <42141895+djlim98@users.noreply.github.com>
2021-04-23 17:28:32 +0900

    포맷터 적용, 카카오 서버 res 객체 반환으로 수정

--- libs/kakaoWork/index.js
+++ libs/kakaoWork/index.js
@@ -1,15 +1,15 @@
-const Config = require("../../configs/app/development.js");
+const Config = require('../../configs/app/development.js');
 
-const axios = require("axios");
+const axios = require('axios');
 const kakaoInstance = axios.create({
-  baseURL: "https://api.kakaowork.com",
+  baseURL: 'https://api.kakaowork.com',
   headers: {
     Authorization: `Bearer ${Config.keys.kakaoWork.bot}`,
   },
 });
 
 exports.getUserList = async () => {
-  const res = await kakaoInstance.get("/v1/users.list");
+  const res = await kakaoInstance.get('/v1/users.list');
   return res.data.users;
 };
 
@@ -17,7 +17,7 @@ exports.openConversations = async ({ userId }) => {
   const data = {
     user_id: userId,
   };
-  const res = await kakaoInstance.post("/v1/conversations.open", data);
+  const res = await kakaoInstance.post('/v1/conversations.open', data);
   return res.data.conversation;
 };
 
@@ -27,6 +27,6 @@ exports.sendMessage = async ({ conversationId, text, blocks }) => {
     text,
     ...(blocks && { blocks }),
   };
-  const res = await kakaoInstance.post("/v1/messages.send", data);
-  return res.data.message;
+  const res = await kakaoInstance.post('/v1/messages.send', data);
+  return res;
 };
