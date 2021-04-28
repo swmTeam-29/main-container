e404fdde0c35f702cc40c6819ff2c721a12074a8
hojun3775 <hojun3775@gmail.com>
2021-04-28 16:23:54 +0900

    # 테스트용 route API 추가

--- routes/db.js
+++ routes/db.js
@@ -5,6 +5,13 @@ router.get('/', async (req, res) => {
     res.json({result: true});
 })
 
+router.post('/insert', async (req, res) => {
+    console.log(req);
+    const result = await reviewController.insertUserReview(req.body);
+
+    res.json(result);
+})
+
 router.post('/find', async (req, res) => {
     const review = await reviewController.getReviews(req.body);
 
