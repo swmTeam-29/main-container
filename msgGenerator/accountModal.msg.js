8827ae659d91d32ec501509029e3e1b8f18a8dd9
Dongjin Lim <42141895+djlim98@users.noreply.github.com>
2021-04-30 01:27:34 +0900

    Update accountModal.msg.js

--- msgGenerator/accountModal.msg.js
+++ msgGenerator/accountModal.msg.js
@@ -31,17 +31,12 @@ module.exports = () => {
         {
           type: 'label',
           text: '개인정보 제공 동의',
-          markdown: true
+          markdown: true,
         },
         {
           type: 'label',
-          text: 'Team[-2]는 원활한 서비스 제공을 위하여 SWM 홈페이지 계정과 이름, 학력, 거주지 등의 정보를 수집하고 있습니다.',
-          markdown: true
-        },
-        {
-          type: 'label',
-          text: '사용자께서는 개인정보 제공 동의에 거부할 권리가 있습니다. 그러나 서비스 이용을 하실 수 없습니다. SWM 계정을 등록하시는 것으로 정보제공에 동의한 것으로 간주하며 처리할 것입니다. 제공하신 정보는 탈퇴시 삭제되며, 원하는 시기에 언제든지 탈퇴하시면 정보를 삭제하실 수 있습니다.',
-          markdown: true
+          text: 'welcome 메시지에 개인정보수집동의서를 확인해주세요',
+          markdown: true,
         },
         {
           type: 'select',
