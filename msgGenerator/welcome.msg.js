bfa4f2a564b0c9646fb7625120e6423cb2e5ffe8
Dongjin Lim <42141895+djlim98@users.noreply.github.com>
2021-04-30 10:14:04 +0900

    Update welcome.msg.js

--- msgGenerator/welcome.msg.js
+++ msgGenerator/welcome.msg.js
@@ -5,13 +5,28 @@ exports.welcomeMsgGenerator = (conversationId) => {
     blocks: [
       {
         type: 'header',
-        text: '29팀 Team[-2]',
+        text: 'Team[-2]의 애기멘티소마 BOT',
         style: 'blue',
       },
       {
         type: 'text',
-        text: '안녕하세요👋\n멘토링 신청의 모든 것을 도와주는 *애기멘티소마*입니다\n\n듣고 싶던 멘토링을 놓치지 않도록 제가 알림을 보내드릴게요!\n\n소마 계정 정보를 등록해 빠르고 간편하게 신청/취소해보세요 👍\n\n멘토링이 성공적으로 신청이 되면 신청 결과를 보내드립니다. 결과가 올 때까지 잠시만 기다려주세요!\n\n멘토링 한 줄 평도 제가 조회해드릴게요 🔍\n',
+        text:
+          '안녕하세요👋\n멘토링 신청의 모든 것을 도와주는 *애기멘티소마*입니다\n\n듣고 싶던 멘토링을 놓치지 않도록 제가 알림을 보내드릴게요!\n\n소마 계정 정보를 등록해 빠르고 간편하게 신청/취소해보세요 👍\n\n멘토링이 성공적으로 신청이 되면 신청 결과를 보내드립니다. 결과가 올 때까지 잠시만 기다려주세요!\n\n멘토링 한 줄 평도 제가 조회해드릴게요 🔍\n',
         markdown: true,
+      },
+      {
+        type: 'context',
+        content: {
+          type: 'text',
+          text:
+            '[README 보러 가기](https://github.com/swmTeam-29/main-container)',
+          markdown: true,
+        },
+        image: {
+          type: 'image_link',
+          url:
+            'https://t1.kakaocdn.net/kakaowork/resources/block-kit/context/etc@3x.png',
+        },
       },
       {
         type: 'context',
